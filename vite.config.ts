30fb9d01fa1738ba877501281546e713a862f8f7
Dax <daxnavarrete@gmail.com>
2025-09-04 16:06:56 -0500

    chore: update URL path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/workshop-cms-2025/',
+  base: '/workshop-webpage/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
